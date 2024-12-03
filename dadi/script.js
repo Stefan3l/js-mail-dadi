ba08ec9bc24ab9da707baaf3c401b3f5de26e489
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-03 17:12:04 +0100

    feat: ex dadi

---
+++ dadi/script.js
@@ -0,0 +1,27 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// 1 - Creea un array con i numeri da 1 a 6
+// 2 - Genero un numero random dal computer
+// 3 - Genero un numero random dal utente
+// 4 - Stabilisco un vincitore in base al punteggio piu alto
+
+const number = [1, 2, 3, 4, 5, 6]
+
+const numbCpu =  Math.floor((Math.random() * 6) + 1);
+
+const numbUtente = Math.floor((Math.random() * 6) + 1) ;
+
+console.log(numbCpu, numbUtente)
+
+if ( numbCpu > numbUtente) {
+    alert(`ha vinto il CPU con il numero ${numbCpu} 😎`)
+}
+else if ( numbUtente > numbCpu) {
+    alert(`Ha vinto il utente con il numero ${numbUtente} 😎`)
+}
+else {
+    alert(`CPU con il numero ${numbCpu} e pari al numero ${numbUtente} del utente 😁`)
+}
